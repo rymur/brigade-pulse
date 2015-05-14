6fff71af774916989033ee1626ef19db190b42a4
Bryan Finlayson <bdfinlayson@gmail.com>
2015-05-14 06:57:10 -0500

    remove unused code. Fix formatting in navbar.js

--- app/directives/navbar.js
+++ app/directives/navbar.js
@@ -1,30 +1,29 @@
 angular
-  .module('project-x.directives.navbar', [])
-  .directive('navbar', function() {
-    'use strict';
-    return {
-      restrict: 'E',
-      scope: {
-        data: '='
-      },
-      templateUrl: 'app/templates/navbar.html',
-      controller: function($scope, $rootScope, $location) {
-  $rootScope.$on('$locationChangeSuccess', function(){
-    var isBrigade = $location.path().indexOf('brigades') !== -1;
-    var isProject = $location.path().indexOf('projects') !== -1;
-    var isHome = $location.path() == "/"
-   if (isBrigade) {
-    $('.nav li').removeClass('active')
-    $('.nav li:nth-child(2)').addClass('active')
-  } else if (isProject) {
-    $('.nav li').removeClass('active')
-    $('.nav li:nth-child(2)').addClass('active')
-  } else {
-    $('.nav li').removeClass('active')
-    $('.nav li:nth-child(1)').addClass('active')
-  }
+.module('project-x.directives.navbar', [])
+.directive('navbar', function() {
+  'use strict';
+  return {
+    restrict: 'E',
+    scope: {
+      data: '='
+    },
+    templateUrl: 'app/templates/navbar.html',
+    controller: function($scope, $rootScope, $location) {
+      $rootScope.$on('$locationChangeSuccess', function(){
+        var isBrigade = $location.path().indexOf('brigades') !== -1;
+        var isProject = $location.path().indexOf('projects') !== -1;
+        if (isBrigade) {
+          $('.nav li').removeClass('active')
+          $('.nav li:nth-child(2)').addClass('active')
+        } else if (isProject) {
+          $('.nav li').removeClass('active')
+          $('.nav li:nth-child(2)').addClass('active')
+        } else {
+          $('.nav li').removeClass('active')
+          $('.nav li:nth-child(1)').addClass('active')
+        }
 
-  })
-      }
-    };
-  });
+      })
+    }
+  };
+});
