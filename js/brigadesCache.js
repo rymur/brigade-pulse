5f25c5a3428e75c4a976e8b22ce6d3a34d9f7816
beck <beck@eventbrite.com>
2015-06-20 23:30:54 -0500

    totatl_projects property now sent to firebase

--- js/brigadesCache.js
+++ js/brigadesCache.js
@@ -8,7 +8,7 @@
       url: cfa,
       success: function(data){
         data.objects.forEach(function(brigade){
-          brigadeData.push({'name': brigade.name, 'projects_url': brigade.all_projects})
+          brigadeData.push({'name': brigade.name, 'projects_url': brigade.all_projects, 'total_projects': 'n/a'})
         })
       _getBrigadeProjects(brigadeData);
       }
@@ -16,23 +16,26 @@
   }())
 
   function _getBrigadeProjects(brigades){
-    brigades.forEach(function(brigade){
+    brigades.forEach(function(brigade, index){
       //console.log(brigade)
       $.get(brigade.projects_url, function(projects){
-        brigade["total_projects"] = projects.total.toString();
+        brigade['total_projects'] = projects.total.toString();
       })
     })
 
     setTimeout(function(){
       _cacheBrigadesToFirebase(brigadeData);
-    }, 500)
+    }, 10000)
   }
 
   function _cacheBrigadesToFirebase(){
-    console.log(brigadeData)
-    console.log(JSON.stringify(brigadeData));
-    // $.post("https://cfn-brigadepulse.firebaseio.com/brigadeInfo.json", brigadeData ,function(data){
-    //    console.log(brigadeData);
-    // })
+    JSONBrigadeData = JSON.stringify(brigadeData);
+    $.ajax({
+      type: 'POST', // Use POST with X-HTTP-Method-Override or a straight PUT if appropriate.
+      dataType: 'json', // Set datatype - affects Accept header
+      url: "https://cfn-brigadepulse.firebaseio.com/brigadeInfo.json", // A valid URL
+      headers: {"X-HTTP-Method-Override": "PUT"}, // X-HTTP-Method-Override set to PUT.
+      data: JSONBrigadeData // Some data e.g. Valid JSON as a string
+    });
   }
 })();
